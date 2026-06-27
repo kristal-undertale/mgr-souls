81c624b4ef65b5034a33f525203e6d438fe3f8a0
DiamondBor <99621601+DiamondBor@users.noreply.github.com>
2026-06-27 11:41:39 -0300

    p

--- lib.lua
+++ lib.lua
@@ -1,13 +1,3 @@
 local Lib = {}
 
-function Lib:assetsMonsterSoulCheckOverwrite(path)
-    if StringUtils.sub(path, 1, 7) == "player/" and Game:getMonsterSoul() then
-        path = StringUtils.sub(path, 1, 7) .. "monster" .. "/" .. StringUtils.sub(path, 8)
-    elseif StringUtils.sub(path, 1, 8) == "!player/" then
-        return StringUtils.sub(path, 2)
-    end
-
-    return path
-end
-
 return Lib
